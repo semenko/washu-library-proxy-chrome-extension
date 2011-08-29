7005bed894b7ba0a04eadf3c749a3d2e7089c578
Nick Semenkovich <semenko@alum.mit.edu>
2011-08-28 22:31:02 -0500

    Change update paths, actually grep correct file...

--- scrape-nicely/make-json.sh
+++ scrape-nicely/make-json.sh
@@ -9,24 +9,24 @@ grep -Fxv -f DANFORTH.txt BECKER.txt | grep -v -f BLACKLIST.txt > BECKER-UNIQUE.
 grep -Fxv -f BECKER.txt DANFORTH.txt | grep -v -f BLACKLIST.txt > DANFORTH-UNIQUE.txt
 
 echo -n "var intersect_journals = {
-    \"list\": ['" > intersect-journals.js
-cat INTERSECT.txt | tr "\n" "," | sed "s/,/','/g" | head -c-2 >> intersect-journals.js
+    \"list\": ['" > ../intersect-journals.js
+cat INTERSECT.txt | tr "\n" "," | sed "s/,/','/g" | head -c-2 >> ../intersect-journals.js
 echo "]
-};" >> intersect-journals.js
+};" >> ../intersect-journals.js
 
 
 echo -n "var becker_journals = {
-    \"list\": ['" > becker-journals.js
-cat BECKER.txt | tr "\n" "," | sed "s/,/','/g" | head -c-2 >> becker-journals.js
+    \"list\": ['" > ../becker-journals.js
+cat BECKER-UNIQUE.txt | tr "\n" "," | sed "s/,/','/g" | head -c-2 >> ../becker-journals.js
 echo "]
-};" >> becker-journals.js
+};" >> ../becker-journals.js
 
 
 echo -n "var danforth_journals = {
-    \"list\": ['" > danforth-journals.js
-cat DANFORTH.txt | tr "\n" "," | sed "s/,/','/g" | head -c-2 >> danforth-journals.js
+    \"list\": ['" > ../danforth-journals.js
+cat DANFORTH-UNIQUE.txt | tr "\n" "," | sed "s/,/','/g" | head -c-2 >> ../danforth-journals.js
 echo "]
-};" >> danforth-journals.js
+};" >> ../danforth-journals.js
 
 
 # Finals structure should look like:
