a52ebe72515cf3e23d9325955490e104534086ea
Nick Semenkovich <semenko@alum.mit.edu>
2011-08-28 22:58:21 -0500

    Danforth's URL listing has two different formats, might remove first (? open access?)

--- scrape-nicely/scrape-danforth.sh
+++ scrape-nicely/scrape-danforth.sh
@@ -5,6 +5,7 @@
 curl "http://je5qh2yg7p.search.serialssolutions.com/" >> danforth-raw.html
 
 grep -oE "U=http[^\"]+" danforth-raw.html | grep -oE "%3A%2F%2F.*" | grep -oE "^%3A%2F%2F[^%]+" | cut -b10- > danforth-pre-unique.txt
+grep -oE "url%3Dhttp[^\"]+" danforth-raw.html | grep -oE "%3A%2F%2F.*" | grep -oE "^%3A%2F%2F[^%]+" | cut -b10- >> danforth-pre-unique.txt
 wc -l danforth-pre-unique.txt
 sort danforth-pre-unique.txt | tr "[A-Z]" "[a-z]" | uniq > DANFORTH.txt
 wc -l DANFORTH.txt
