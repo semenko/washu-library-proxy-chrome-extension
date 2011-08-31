e5fe5737ee62ac14ae7569d02497dab38f15a5cb
Nick Semenkovich <semenko@alum.mit.edu>
2011-08-30 19:15:39 -0500

    v0.7 beta packaged

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a zip of the extensions
-zip builds/wulib-vN.N.zip images/*.png javascript/*.js *.json views/*.html
+zip builds/wulib-vN.N.zip images/*.png javascript/*.js views/*.html manifest.json
