919189f390a1e4b8b72f8bab24a3be97e1b3aeb7
Nick Semenkovich <semenko@alum.mit.edu>
2011-09-12 23:29:23 -0500

    Changes to leave BECKER.txt and DANFORTH.txt untouched.

--- scrape-nicely/make-json.sh
+++ scrape-nicely/make-json.sh
@@ -4,25 +4,24 @@
 #  Intermediates: BECKER-UNIQUE.txt, INTERSECT.txt, DANFORTH-UNIQUE.txt
 # Output: becker-journals.js, intersect-journals.js, danforth-journals.js
 
-# Add in the whitelists
-cat INTERSECT-WHITELIST.txt >> BECKER.txt
-cat INTERSECT-WHITELIST.txt >> DANFORTH.txt
-
-# Sort pre-intersect
-sort BECKER.txt -o BECKER.txt
-sort DANFORTH.txt -o DANFORTH.txt
+# Sort & uniq, just in case.
+sort -u BECKER.txt -o BECKER.txt
+sort -u DANFORTH.txt -o DANFORTH.txt
 
 grep -Fx -f DANFORTH.txt BECKER.txt | grep -v -f BLACKLIST.txt | uniq > INTERSECT.txt
-grep -Fxv -f DANFORTH.txt BECKER.txt | grep -v -f BLACKLIST.txt | uniq > BECKER-UNIQUE.txt
-grep -Fxv -f BECKER.txt DANFORTH.txt | grep -v -f BLACKLIST.txt | uniq > DANFORTH-UNIQUE.txt
+cat INTERSECT-WHITELIST.txt >> INTERSECT.txt
+sort -u INTERSECT.txt -o INTERSECT.txt
+
+grep -Fxv -f DANFORTH.txt BECKER.txt | grep -v -f BLACKLIST.txt | grep -v -f INTERSECT.txt | uniq > BECKER-UNIQUE.txt
+grep -Fxv -f BECKER.txt DANFORTH.txt | grep -v -f BLACKLIST.txt | grep -v -f INTERSECT.txt | uniq > DANFORTH-UNIQUE.txt
 
 # Individual whitelists, so we don't break intersects
 cat BECKER-WHITELIST.txt >> BECKER-UNIQUE.txt
 cat DANFORTH-WHITELIST.txt >> DANFORTH-UNIQUE.txt
 
 # Sort again
-sort BECKER-UNIQUE.txt -o BECKER-UNIQUE.txt
-sort DANFORTH-UNIQUE.txt -o DANFORTH-UNIQUE.txt
+sort -u BECKER-UNIQUE.txt -o BECKER-UNIQUE.txt
+sort -u DANFORTH-UNIQUE.txt -o DANFORTH-UNIQUE.txt
 
 
 echo -n "var intersect_journals = {
