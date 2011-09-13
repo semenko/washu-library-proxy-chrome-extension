c46cb2a88c7eb3a6daea7cb4641952da59ffbc6d
Nick Semenkovich <semenko@alum.mit.edu>
2011-09-13 00:35:17 -0500

    v1.0.6 packaged
    Taking a page from the Chromium versioning handbook :P

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a zip of the extensions
-zip builds/wulib-vN.N.zip images/*.png javascript/*.js views/*.html manifest.json
+zip builds/wulib-vN.N.N.zip images/*.png javascript/*.js views/*.html manifest.json
