3a37bfa902480e76171ad42688ea3ef6270c9fa3
Nick Semenkovich <semenko@alum.mit.edu>
2011-09-12 19:01:14 -0500

    Large update to Libproxy (Danforth) supported URLs.
    The e-Journals list @ Danforth contains lots of open/free sites (that the proxy does NOT support).

--- scrape-nicely/scrape-danforth.sh
+++ scrape-nicely/scrape-danforth.sh
@@ -4,8 +4,8 @@
 
 curl "http://je5qh2yg7p.search.serialssolutions.com/" >> danforth-raw.html
 
-grep -iv "open" danforth-raw.html | grep -oE "U=http[^\"]+" | grep -oE "%3A%2F%2F.*" | grep -oE "^%3A%2F%2F[^%]+" | cut -b10- > danforth-pre-unique.txt
-grep -iv "open" danforth-raw.html | grep -oE "url%3Dhttp[^\"]+" | grep -oE "%3A%2F%2F.*" | grep -oE "^%3A%2F%2F[^%]+" | cut -b10- >> danforth-pre-unique.txt
+grep -iv "open" danforth-raw.html | grep "libproxy" | grep -oE "U=http[^\"]+" | grep -oE "%3A%2F%2F.*" | grep -oE "^%3A%2F%2F[^%]+" | cut -b10- > danforth-pre-unique.txt
+grep -iv "open" danforth-raw.html | grep "libproxy" | grep -oE "url%3Dhttp[^\"]+" | grep -oE "%3A%2F%2F.*" | grep -oE "^%3A%2F%2F[^%]+" | cut -b10- >> danforth-pre-unique.txt
 wc -l danforth-pre-unique.txt
 sort danforth-pre-unique.txt | tr "[A-Z]" "[a-z]" | uniq > DANFORTH.txt
 wc -l DANFORTH.txt
