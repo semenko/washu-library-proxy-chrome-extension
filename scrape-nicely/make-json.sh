004766052bf7574af017066c933c8829dfb96f12
Nick Semenkovich <semenko@alum.mit.edu>
2011-09-12 19:41:46 -0500

    Better whitelist support, CHEMnetBASE changes.

--- scrape-nicely/make-json.sh
+++ scrape-nicely/make-json.sh
@@ -7,16 +7,22 @@
 # Add in the whitelists
 cat INTERSECT-WHITELIST.txt >> BECKER.txt
 cat INTERSECT-WHITELIST.txt >> DANFORTH.txt
-cat BECKER-WHITELIST.txt >> BECKER.txt
-cat DANFORTH-WHITELIST.txt >> DANFORTH.txt
 
-# Sort post intersect
+# Sort pre-intersect
 sort BECKER.txt -o BECKER.txt
 sort DANFORTH.txt -o DANFORTH.txt
 
 grep -Fx -f DANFORTH.txt BECKER.txt | grep -v -f BLACKLIST.txt | uniq > INTERSECT.txt
 grep -Fxv -f DANFORTH.txt BECKER.txt | grep -v -f BLACKLIST.txt | uniq > BECKER-UNIQUE.txt
 grep -Fxv -f BECKER.txt DANFORTH.txt | grep -v -f BLACKLIST.txt | uniq > DANFORTH-UNIQUE.txt
+
+# Individual whitelists, so we don't break intersects
+cat BECKER-WHITELIST.txt >> BECKER-UNIQUE.txt
+cat DANFORTH-WHITELIST.txt >> DANFORTH-UNIQUE.txt
+
+# Sort again
+sort BECKER-UNIQUE.txt -o BECKER-UNIQUE.txt
+sort DANFORTH-UNIQUE.txt -o DANFORTH-UNIQUE.txt
 
 
 echo -n "var intersect_journals = {
