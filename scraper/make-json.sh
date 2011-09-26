399255e405da54df93cc9c9bee90c34e4e002115
Nick Semenkovich <semenko@alum.mit.edu>
2011-09-25 20:42:15 -0500

    Optimized hot code path: use hasOwnProperty() over Array.indexOf()
    TODO: Consider direct objects, see: http://jsperf.com/lists-indexof-vs-in-operator

--- scraper/make-json.sh
+++ scraper/make-json.sh
@@ -8,28 +8,20 @@ sort -u becker.txt -o becker.txt
 sort -u danforth.txt -o danforth.txt
 sort -u intersect.txt -o intersect.txt
 
-echo -n "var intersect_journals = {
-    \"list\": ['" > ../javascript/journals.js
-cat intersect.txt | sort | uniq | tr "\n" "," | sed "s/,/','/g" | head -c-2 >> ../javascript/journals.js
-echo "]
-};" >> ../javascript/journals.js
+echo -n "var intersect_journals = {'" > ../javascript/journals.js
+cat intersect.txt | sort | uniq | tr "\n" "," | sed "s/,/':1,'/g" | head -c-2 >> ../javascript/journals.js
+echo "};" >> ../javascript/journals.js
 
 
-echo -n "var becker_journals = {
-    \"list\": ['" >> ../javascript/journals.js
-cat becker.txt | sort | uniq | tr "\n" "," | sed "s/,/','/g" | head -c-2 >> ../javascript/journals.js
-echo "]
-};" >> ../javascript/journals.js
+echo -n "var becker_journals = {'" >> ../javascript/journals.js
+cat becker.txt | sort | uniq | tr "\n" "," | sed "s/,/':1,'/g" | head -c-2 >> ../javascript/journals.js
+echo "};" >> ../javascript/journals.js
 
 
-echo -n "var danforth_journals = {
-    \"list\": ['" >> ../javascript/journals.js
-cat danforth.txt | sort | uniq | tr "\n" "," | sed "s/,/','/g" | head -c-2 >> ../javascript/journals.js
-echo "]
-};" >> ../javascript/journals.js
+echo -n "var danforth_journals = {'" >> ../javascript/journals.js
+cat danforth.txt | sort | uniq | tr "\n" "," | sed "s/,/':1,'/g" | head -c-2 >> ../javascript/journals.js
+echo -n "};" >> ../javascript/journals.js
 
 
 # Final structure should look like:
-#var intersect_journals = {
-#    "list": []
-#};
+#var intersect_journals = {'url':1,'url':1,...};
