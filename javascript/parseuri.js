213aac6197bae27312d8c175689b0aedae43253e
Nick Semenkovich <semenko@alum.mit.edu>
2011-09-26 18:42:30 -0500

    Bugfix, drop two more parameters.

--- javascript/parseuri.js
+++ javascript/parseuri.js
@@ -6,25 +6,21 @@ function parseUri (str) {
 	var	o   = parseUri.options,
 		m   = o.parser["strict"].exec(str),
 		uri = {},
-		i   = 12;
+		i   = 10;
 
 	while (i--) uri[o.key[i]] = m[i] || "";
-
 	uri[o.q.name] = {};
-	uri[o.key[10]].replace(o.q.parser, function ($0, $1, $2) {
-		if ($1) uri[o.q.name][$1] = $2;
-	});
 
 	return uri;
 }
 
 parseUri.options = {
-	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file"],
+	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path"],
 	q:   {
 		name:   "queryKey",
 		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
 	},
 	parser: {
-		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?)/
+		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*)))/
 	}
 };
